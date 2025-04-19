2302888fc745060fab3db74c58d40ee72e5d32c7
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2025-04-19 19:24:00 +0530

    trying to make the basic time with ts and socket.io

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,19 +4,27 @@ import { useState } from 'react'
 import React from 'react'
 import io, { Socket } from 'socket.io-client'
 function App: React.FC=() =>{
-const [value, setvalue] = useState(null)
-const socket:Socket<ServerToTlientEvents, clientToServerEvents>=io({axios: {origin: http://localhost:9999}})
+const [state, setstate] = useState(0)
+const socket:Socket<ServerToTlientEvents, clientToServerEvents>=io({axios: {origin: "http://localhost:9999 ""}, withCredientials: true })
   interface increaMent{
     increase():()=>number
   }
   interface decreaMeent{
     decrease():()=>number
   }
+
+  function increase(setincrease: increaMent){
+    setstate+=1;
+  }
+  function decrease(setdecrease: decreaMent){
+    setstate-=1;
+  }
+
   return (
     <>
-  <div></div>
-  <div>+</div>
-  <div>-</div>
+  <div>{value}</div>
+  <div onClick={decrease}>+</div>
+  <div onClick={increase}>-</div>
     </>
   )
 }
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,34 +1,43 @@
-import express from "express"
+import {express} from "express"
 import {Server} from "socket.io"
 import cors from "cors"
 import { createServer } from "http"
-import { callbackify } from "util"
-import { eventNames } from "process"
+
 const app=express()
-const server=createServer(app)
+const httpServer=createServer(app)
 app.use(cors())
 // app.use()
-const io=new Server(server, {
+const io=new Server(httpServer, {
  cors:{ 
   origin: "http://localhost:5173",
   methods: ["GET", "POST"],
   credentials: true
  }
 })
-io.on('connection', ('response': string)=>{
-  console.log("connection is successfully made");
-})
+interface clientType{
+  clientMessage: string
+}
+io.on("connection", (socket)=>{
+  console.log("connection is successfully made.");
+  socket.on('chat', (message: string)=>{
+    console.log("Recieve message is:", message);
+    // io.on("ressponse", message) //this is not corerct i am trying to understand why is the error.
+    emit.on("ressponse", message)
+  })
+  socket.on("client", (clientMessage: clientType)=>{
+    console.log("Client message that he is sending is:", clientMessage);
+  })
 
-io.disconnectSockets('disconnect', ('disconne')=> {
-  console.log("Disconencted successfully");
+  socket.on('disconnect', ()=> {
+    console.log("Disconencted successfully");
+  })
+  
+  // socket.emit('client', ('clientMessage'):string=>{
+  //   console.log(`clientMessage is ${clientMessage}`);
+  // })
+  // socket.on(eventNames, callbackify)
 })
-
-io.emit('client', ('clientMessage')=>{
-  console.log(`clientMessage is ${clientMessage}`);
-})
-io.on(eventNames, callbackify)
-
 const path=9999;
-server.listen(path, ()=>{
+httpServer.listen(path, ()=>{
   console.log(`Path is listening to http://localhost:${path}`);
 })
